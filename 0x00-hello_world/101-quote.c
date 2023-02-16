095b64a8f1a0c803b79c13a28fb0a0eb74e085e2
elhanabullahi <elhanabullahigmail.com>
2023-02-16 14:04:44 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- * Return Always 1
+ * main - prints excatly and that piece of art is useful
+ * Return: Always 1 if succsess
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
