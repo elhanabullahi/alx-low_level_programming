e54f1751f8c045e3216dba84810e051ec2cd42e4
elhanabullahi <elhanabullahigmail.com>
2023-03-01 21:52:42 -0800

    function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,8 +4,8 @@
  * _strcmp -function that compares two strings
  * @s1: first string to compare
  * @s2: second string to compare
- * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * more than 0 if s1 is greater than s2
+ * Return: less than 0 if S1 is less than S2, 0 if they're equal,
+ * more than 0 if S1 is greater than S2
  */
 
 int _strcmp(char *s1, char *s2)
