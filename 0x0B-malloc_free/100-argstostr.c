0b999c009764b3b11f2735b12490752b84726cb7
elhanabullahi <elhanabullahigmail.com>
2023-03-14 05:58:37 -0700

    concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,49 +3,44 @@
 #include "main.h"
 
 /**
- * argstostr - function declaration
- * @ac: no of args
- * @av: array of pointer
- * Return: char type
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
  */
-
-char *argstostr(int ac, char **av);
-
-/**
- * argstostr - function definition
- * @ac: number of arguments
- * @av: array of pointers to argumnets
- * Description: Return NULL of pointer to string
- * Return: NULL or a pointer to a string
- */
-
 char *argstostr(int ac, char **av)
 {
-	int row, column, len = 0, total_len = 0;
 	char *str;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (row = 0; row < ac; row++)
+	for (arg = 0; arg < ac; arg++)
 	{
-		len = strlen(av[row]);
-		total_len += len + 1;
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
 
-	str = (char *) malloc(total_len * sizeof(char));
+	str = malloc(sizeof(char) * size + 1);
+
 	if (str == NULL)
 		return (NULL);
 
-	len = 0;
-	for (row = 0; row < ac; row++)
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		for (column = 0; av[row][column] != '\0'; column++)
-		{
-			str[len++] = av[row][column];
-		}
-		str[len++] = '\n';
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
 	}
-	str[len] = '\0';
+
+	str[size] = '\0';
+
 	return (str);
 }
