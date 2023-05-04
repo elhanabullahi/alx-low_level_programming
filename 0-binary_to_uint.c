1c3ec48bdc446c437ba31b71b6679e861461eb94
Elhan Ali <elhanabullahi@gmail.com>
2023-05-04 12:58:04 -0700

    convert binary num to an unsigned int

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Binary number to convert.
+ *
+ * Return: Converted unsigned int, or 0 if b is NULL or contains non-binary.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		if (b[len] & 1)
+			ui += base_two;
+	}
+
+	return (ui);
+} /*elhan*/
--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN__H
-#define _MAIN__H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
