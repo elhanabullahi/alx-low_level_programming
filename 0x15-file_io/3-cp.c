012e4163f8bcae9d0e86b4c2691512050e8bb717
Elhan Ali <elhanabullahi@gmail.com>
2023-05-08 10:55:07 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,71 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Program that copies the content of a file to another file
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Description:
- * Usage: cp file_from file_to
- *
- * Return: 0 on success
+ * error_file - program checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-int main(int argc, char **argv)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	int f_from, f_to, rf, wf;
-	char buff[1024];
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-
-	f_from = open(argv[1], O_RDONLY);
-	if (f_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-	f_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (f_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	while (1)
 	{
-		rf = read(f_from, buff, 1024);
-
-		if (rf == 0)
-			break;
-		else if (rf == -1)
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-		wf = write(f_to, buff, rf);
-		if (wf == -1)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	if (close(f_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_from), exit(100);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	if (close(f_to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_to), exit(100);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
-} /*elhan*/
+}/*elhan*/
